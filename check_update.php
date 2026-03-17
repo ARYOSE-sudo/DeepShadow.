482e0347812469bcd184a0970399c175af0dd220
RYOSO <kakachifreefire18@gmail.com>
2026-03-17 01:01:49 +0000

    Add files via upload

---
+++ check_update.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+echo json_encode([
+    "app_version"  => "1.0",
+    "has_update"   => true,
+    "download_url" => "http://localhost/cyber/st-setup-1.8.30.exe",
+    "data_version" => "1",
+    "data_url"     => "http://localhost/cyber/data.json"
+]);
+?>
